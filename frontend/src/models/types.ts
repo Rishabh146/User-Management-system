e227fe2f460026db2c39a1f254254511a44882a4
Rishabh <rishabh.gautam@neuralfoundry.co.uk>
2025-04-07 17:23:02 +0530

    Create a base type extend, pick or omit as required Make the snack bar consistent through out all pages

--- frontend/src/models/types.ts
+++ frontend/src/models/types.ts
@@ -1,34 +1,24 @@
-
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  age: number;
-  gender: string; 
-  token: string;
-}
-export interface updateProfileType{
-  name: string;
-  email: string;
-  age?: number;
-  gender: string; 
-}
-export interface RegisterUser {
-  name: string;
-  email: string;
-  age: number;
-  gender: string; 
-  token?: string;
-}
-
-export interface UserInfoType {
-  payload: string;
-  _id: string;
-  status: string;
-  id: number;
+export interface BaseUser {
   name: string;
   email: string;
   age: number;
   gender: string;
-  token: string; 
+}
+
+export interface User extends BaseUser {
+  id: number;
+  token: string;
+}
+
+export interface updateProfileType extends Omit<BaseUser, 'age'> {
+  age?: number;
+}
+
+export interface RegisterUser extends BaseUser {
+  token?: string;
+}
+
+export interface UserInfoType extends User {
+  _id: string;
+  status: string;
 }
